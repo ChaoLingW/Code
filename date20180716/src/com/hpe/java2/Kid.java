cf0e2e1d1b5f52654bb609022f94fe78d9620bef
chaolingw@163.com <chaolingw@163.com>
2018-07-16 23:36:32 +0800

    封装 static关键字 this关键字 super final 多态

---
+++ date20180716/src/com/hpe/java2/Kid.java
@@ -0,0 +1,10 @@
+package com.hpe.java2;
+
+public class Kid extends Person{
+	
+	@Override
+	public void eat() {
+		System.out.println("小孩吃饭");
+	}
+
+}
---
+++ date20180716/src/com/hpe/java2/Women.java
@@ -0,0 +1,14 @@
+package com.hpe.java2;
+
+public class Women extends Person{
+
+	@Override
+	public void eat() {
+		System.out.println("女人吃饭要狼吞虎咽");
+	}
+	
+	public void makrUp(){
+		
+		System.out.println("makeuping");
+	}
+}
