cb07df2e68bab543efbb58dbd1e064a742636aa5
chaolingw@163.com <chaolingw@163.com>
2018-07-16 23:49:06 +0800

    虾扯蛋问题有意思

---
+++ homeWork/src/com/hpe/java/day0716/Chess.java
@@ -0,0 +1,16 @@
+package com.hpe.java.day0716;
+
+/**
+ * 
+ * @author chaoling
+ * @date 2018年7月16日下午6:45:39
+ * @Description
+ */
+public class Chess {
+
+	
+	
+	public void rule(){
+		System.out.println("棋。Chess");
+	}
+}
---
+++ homeWork/src/com/hpe/java/day0716/ChineseChess.java
@@ -0,0 +1,15 @@
+package com.hpe.java.day0716;
+/**
+ * 
+ * @author chaoling
+ * @date 2018年7月16日下午6:59:02
+ * @Description
+ */
+public class ChineseChess extends Chess{
+	
+	@Override
+	public void rule() {
+		System.out.println("中国象棋。Chinese Chess");
+	}
+
+}
