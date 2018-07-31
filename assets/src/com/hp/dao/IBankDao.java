55cba4ef0be8633712f6c369705054070c2bf3f6
chaolingw@163.com <chaolingw@163.com>
2018-07-31 18:55:18 +0800

    删除AdminDaoImpl

--- assets/src/com/hp/dao/IBankDao.java
+++ assets/src/com/hp/dao/IBankDao.java
@@ -9,35 +9,35 @@ import com.hp.po.Bank;
  * @author chaoling
  * @date 2018年7月30日
  */
-public interface BankDao {
+public interface IBankDao {
 	
 	/**
-	 * @Description: 对bank表执行插入操作
+	 * @Description: 添加一条记录
 	 * @param bank
-	 * @return true 添加成功; false 添加失败  
+	 * @return >0 添加成功
 	 * @throws SQLException
 	 */
-	public boolean add(Bank bank);
+	public int addBank(Bank bank);
 	
 	/**
-	 * @Description: 对bank表执行删除操作
-	 * @param bank
-	 * @return true 删除成功; false 删除失败   
+	 * @Description: 根据银行id删除改银行
+	 * @param bankId
+	 * @return >0 删除成功
 	 * @throws SQLException
 	 */
-	public boolean delete(Bank bank);
+	public int deleteBankByBankId(int bankId);
 	
 	/**
 	 * @Description: 对bank表执行更新/修改操作
 	 * @param bank
-	 * @return true 更新成功; false 更新失败
+	 * @return >0 更新成功
 	 * @throws SQLException
 	 */
-	public boolean update(Bank bank);
+	public int updateBankByBankId(Bank bank);
 	
 	/**
 	 * @Description: 查询bank表中的所有记录
-	 * @return List<bank> 存储所有的记录，为空时且size()为0时表示表为空  
+	 * @return banks 存储所有的记录，为空时且size()为0时表示表为空  
 	 * @throws SQLException
 	 */
 	public List<Bank> selectAll();
@@ -45,15 +45,15 @@ public interface BankDao {
 	/**
 	 * @Description: 根据id查询是否存在该id的记录
 	 * @param bankId
-	 * @return true 存在; false 不存在   
+	 * @return >0 存在   
 	 * @throws SQLException
 	 */
-	public boolean selectById(int bankId);
+	public int selectByBankId(int bankId);
 
 	/**
 	 * @Description: 根据name查询是否存在该名称
 	 * @param name
-	 * @return true 存在; false 不存在   
+	 * @return bank 存在返回这条记录，不存在返回null
 	 * @throws SQLException
 	 */
 	public Bank selectByName(String name);
